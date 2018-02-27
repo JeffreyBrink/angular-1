8721289a81f2607ad481f1214e196614d5f70ec2
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-27 10:17:23 -0700

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: "angular-1-app",
-	templateUrl: "./templates/angular5-example-app.html"
+	templateUrl: (".app.component.html")
 })
 
 export class AppComponent {}
